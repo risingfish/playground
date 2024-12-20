6a691ce0fdf26fdf494944fa3a84cca87f48bfa5
Zac f <risingfish@gmail.com>
2024-12-20 15:47:26 -0700

    Improved chocolate problem by adding dynamic programming.

--- codeTests/chocolate.js
+++ codeTests/chocolate.js
@@ -1,18 +1,31 @@
-function findMinWeight(weights, d, count = 0) {
+// Recursion
+function findMinWeightRecursive(weights, d, count = 0) {
     // Write your code here
-
-    // sort descending
     const sortedArray = weights.sort().reverse();
-
     sortedArray[0] = Math.floor(sortedArray[0] / 2);
 
     if (count >= d) {
         return weights.reduce((accumulator, current) => accumulator + current, 0);
     } else {
-        return findMinWeight(sortedArray, d, ++count);
+        return findMinWeightRecursive(sortedArray, d, ++count);
     }
 }
 
+// "Dynamic" programming
+function findMinWeightsDynamic(weights, d) {
+    const workArray = [...weights];
 
-const result = findMinWeight([30, 20, 25], 5);
-console.log(result);
+    for (let i = 0; i <= d; i++) {
+        workArray.sort().reverse();
+        workArray[0] = Math.floor(workArray[0] / 2);
+    }
+
+    return workArray.reduce((accumulator, current) => accumulator + current, 0);
+}
+
+const testArray = [30, 20, 25];
+const days = 8;
+
+console.log(findMinWeightsDynamic(testArray, days));
+console.log(findMinWeightRecursive(testArray, days));
+
