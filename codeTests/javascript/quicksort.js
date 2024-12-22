5856d1f4b285260ac23163090dd277a0c7d81891
Zac f <risingfish@gmail.com>
2024-12-22 09:53:23 -0700

    Added quicksort.js

---
+++ codeTests/javascript/quicksort.js
@@ -0,0 +1,35 @@
+// Random number generator
+function genArray(len, max = 100) {
+    const result = []
+    for (let i = 0; i < len; i++) {
+        result.push(Math.floor(Math.random() * max))
+    }
+    return result;
+}
+
+// Just a counter to see recursion count
+let rec = 0;
+
+/**
+ * BAsic quicksort function in Javascript
+ * @param arr
+ * @return {*|*[]}
+ */
+function quicksort(arr) {
+    rec++;
+    // base case
+    if (arr.length <= 1) return arr;
+
+    const pivot = arr[Math.floor(arr.length / 2)];
+    const low = arr.filter(x => x < pivot);
+    const high = arr.filter(x => x > pivot);
+    return [...quicksort(low), pivot, ...quicksort(high)];
+}
+
+
+
+const arr = genArray(100);
+const start = new Date();
+const end = (new Date()).getTime() - start.getTime();
+console.log(quicksort(arr), rec);
+console.log('Took ' + end + 'ms to run');
