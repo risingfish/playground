8c4740a2697fbb01421551fc0789bb5cb564bc0f
Zac f <risingfish@gmail.com>
2025-01-02 10:43:43 -0700

    Python class

--- learning/python/basics/exceptions.py
+++ learning/python/basics/exceptions.py
@@ -1,3 +1,5 @@
+from timeit import timeit
+
 numbers = [1, 2]
 
 try:
@@ -5,10 +7,10 @@ try:
 except IndexError:
     print("Index not in the the list")
 
-try:
-    age = int(input("Age: "))
-except ValueError:
-    print("Age must be an integer")
+# try:
+#     age = int(input("Age: "))
+# except ValueError:
+#     print("Age must be an integer")
 
 file = None
 try:
@@ -17,3 +19,30 @@ except FileNotFoundError:
     print("File could not be opened")
 finally:
     file.close()
+
+code1 = """
+def calculate_xfactor(age):
+    if age <= 0:
+        raise ValueError("Age must be positive")
+    return 10 / age
+
+
+try:
+    calculate_xfactor(-1)
+except ValueError as error:
+    pass
+"""
+
+code2 = """
+def calculate_xfactor(age):
+    if age <= 0:
+        return None
+    return 10 / age
+
+
+if calculate_xfactor(-1) == None:
+    pass
+"""
+
+print(timeit(code1, number=10000))
+print(timeit(code2, number=10000))
