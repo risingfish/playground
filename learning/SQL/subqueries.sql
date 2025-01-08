3dd7b86a928acb27e01bc65fa0fe943fc36b7e14
Zac f <risingfish@gmail.com>
2025-01-07 22:57:20 -0700

    Whitespace fixing.

--- learning/SQL/subqueries.sql
+++ learning/SQL/subqueries.sql
@@ -12,18 +12,18 @@ from departments d
 # Cleaner and and more efficient version of the query.
 select d.dept_no, d.dept_name, e.first_name as manager_first_name, e.last_name as manager_last_name, dm.from_date
 from departments d
-         join dept_manager dm on d.dept_no = dm.dept_no and dm.from_date < now() and dm.to_date > now()
-         join employees e on dm.emp_no = e.emp_no;
+    join dept_manager dm on d.dept_no = dm.dept_no and dm.from_date < now() and dm.to_date > now()
+    join employees e on dm.emp_no = e.emp_no;
 
 
 # Getting current count of employees for each department
 select d.dept_no, d.dept_name, totals.dep_count
 from departments d
-         join (
-    select sde.dept_no, count(sde.emp_no) as dep_count
-    from dept_emp sde
-    where sde.from_date < now() and sde.to_date > now()
-    group by sde.dept_no) totals on totals.dept_no = d.dept_no;
+    join (
+        select sde.dept_no, count(sde.emp_no) as dep_count
+        from dept_emp sde
+        where sde.from_date < now() and sde.to_date > now()
+        group by sde.dept_no) totals on totals.dept_no = d.dept_no;
 
 
 # explain select d.dept_no, d.dept_name, totals.dep_count
